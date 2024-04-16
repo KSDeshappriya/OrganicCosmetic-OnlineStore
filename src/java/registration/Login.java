493f00a3255efcace25dc5ffac86a8ed38b64289
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-04-16 20:36:10 +0530

    update

--- src/java/registration/Login.java
+++ src/java/registration/Login.java
@@ -14,7 +14,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
