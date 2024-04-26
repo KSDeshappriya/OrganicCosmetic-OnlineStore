001f1308f39a66473d46381ddd0010d12e3ea741
Sim Shad <samsudeenashad@icloud.com>
2024-04-27 02:30:54 +0530

    update
    update

--- src/java/product/CheckoutServlet.java
+++ src/java/product/CheckoutServlet.java
@@ -1,10 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package product;
 
-import java.sql.Blob;
+/**
+ *
+ * @author SIM ASHAD
+ */
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,19 +20,31 @@ import javax.servlet.http.HttpSession;
 import product.product;
 import product.DBConnection;
 
-
-
-
 public class CheckoutServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         List<product> cartProducts = getCartProducts(session);
         request.setAttribute("cartProducts", cartProducts);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Buyer/product/checkout.jsp");
-        dispatcher.forward(request, response);
+        request.getRequestDispatcher("Buyer/product/checkout.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String customer_id  = request.getParameter("customer_id");
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String phone = request.getParameter("phone");
+        String paymentMethod = request.getParameter("paymentMethod");
+
+        HttpSession session = request.getSession();
+        List<product> cartProducts = getCartProducts(session);
+
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.insertOrderIntoDatabase(customer_id, name, address, phone, paymentMethod, cartProducts);
+
+        session.removeAttribute("cart");
+        response.sendRedirect("Buyer/product/orderSuccess.jsp");
     }
 
     private List<product> getCartProducts(HttpSession session) {
@@ -34,6 +53,7 @@ public class CheckoutServlet extends HttpServlet {
         if (cart != null) {
             DBConnection dbConnection = new DBConnection();
             for (int productId : cart) {
+                System.out.println("Fetching product with ID: " + productId);
                 product product = dbConnection.getProductById(productId);
                 if (product != null) {
                     products.add(product);
@@ -42,24 +62,4 @@ public class CheckoutServlet extends HttpServlet {
         }
         return products;
     }
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String name = request.getParameter("name");
-    String address = request.getParameter("address");
-    String payment = request.getParameter("payment");
-
-    // Retrieve the cart products from the session
-    HttpSession session = request.getSession();
-    List<product> cartProducts = getCartProducts(session);
-
-    // Process the order and customer information
-    // (You can add your logic here to save the order, process the payment, etc.)
-
-    // Clear the cart session after successful checkout
-    session.removeAttribute("cart");
-
-    // Redirect to a success page or display a success message
-    response.sendRedirect("success.jsp");
-}
 }
