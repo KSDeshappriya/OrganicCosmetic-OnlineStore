6ada2aeb1cda87cbf7020f39b9bb601827f22e18
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-05-01 09:03:33 +0530

    u

--- src/java/paymentRequest.java
+++ src/java/paymentRequest.java
@@ -7,16 +7,16 @@
  *
  * @author ASUS
  */
-import com.paypal.orders.Amount;
-import com.paypal.orders.OrderRequest;
+//import com.paypal.orders.Amount;
+//import com.paypal.orders.OrderRequest;
 
 public class paymentRequest {
-        public static OrderRequest build(String amount) {
-        OrderRequest orderRequest = new OrderRequest();
-        Amount orderAmount = new Amount();
-        orderAmount.currencyCode("USD");
-        orderAmount.value(amount);
-        orderRequest.amount(orderAmount);
-        return orderRequest;
-    }
+//        public static OrderRequest build(String amount) {
+//        OrderRequest orderRequest = new OrderRequest();
+//        Amount orderAmount = new Amount();
+//        orderAmount.currencyCode("USD");
+//        orderAmount.value(amount);
+//        orderRequest.amount(orderAmount);
+//        return orderRequest;
+//    }
 }
