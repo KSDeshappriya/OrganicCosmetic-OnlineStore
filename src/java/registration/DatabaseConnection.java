246fbbad1d7e67179a59cc6c7e52dc39bff0623b
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-05-01 11:23:26 +0530

    u

--- src/java/registration/DatabaseConnection.java
+++ src/java/registration/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/youtube?useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/OrganicCosmeticStore?useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
 
